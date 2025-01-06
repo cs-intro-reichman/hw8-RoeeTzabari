a079a31a627d5a8d573bb0818f990225e9d377f2
Roee Tzabari <tzabari.roee@gmail.com>
2025-01-06 15:04:06 +0200

    Completed all tasks.

--- Network.java
+++ Network.java
@@ -22,14 +22,15 @@ public class Network {
         userCount = 3;
     }
 
-    public int getUserCount() {
-        return this.userCount;
-    }
     /** Finds in this network, and returns, the user that has the given name.
      *  If there is no such user, returns null.
      *  Notice that the method receives a String, and returns a User object. */
     public User getUser(String name) {
-        //// Replace the following statement with your code
+
+        for (int i = 0; i < userCount; i++) {
+            if (users[i].getName().equals(name)) return users[i];
+        }
+
         return null;
     }
 
@@ -38,7 +39,10 @@ public class Network {
     *  If the given name is already a user in this network, does nothing and returns false;
     *  Otherwise, creates a new user with the given name, adds the user to this network, and returns true. */
     public boolean addUser(String name) {
-        //// Replace the following statement with your code
+        if (userCount < users.length) {
+            users[userCount] = new User(name);
+            userCount++;
+        }
         return false;
     }
 
@@ -46,34 +50,66 @@ public class Network {
      *  If any of the two names is not a user in this network,
      *  or if the "follows" addition failed for some reason, returns false. */
     public boolean addFollowee(String name1, String name2) {
-        //// Replace the following statement with your code
+        User user1 = getUser(name1);
+        User user2 = getUser(name2);
+
+        if (user1 != null && user2 != null) {
+            return user1.addFollowee(name2);
+        }
         return false;
     }
     
     /** For the user with the given name, recommends another user to follow. The recommended user is
      *  the user that has the maximal mutual number of followees as the user with the given name. */
     public String recommendWhoToFollow(String name) {
-        //// Replace the following statement with your code
-        return null;
+        int max = 0;
+        User user = getUser(name);
+        String recommendedUser = null;
+
+        for (int i = 0; i < userCount; i++) {
+            if (!(users[i].getName().equals(name)) && max < users[i].countMutual(user)){
+                max = users[i].countMutual(user);
+                recommendedUser = users[i].getName();
+            }
+        }
+
+        return recommendedUser;
     }
 
     /** Computes and returns the name of the most popular user in this network: 
      *  The user who appears the most in the follow lists of all the users. */
     public String mostPopularUser() {
-        //// Replace the following statement with your code
-        return null;
+        
+        int max = 0;
+        String mostpopular = null;
+
+        for (int i = 0; i < userCount; i++) {
+            String currentUserName = users[i].getName();
+            if (max < followeeCount(currentUserName)) {
+                max = followeeCount(currentUserName);
+                mostpopular = currentUserName;
+            }
+        }
+
+        return mostpopular;
     }
 
     /** Returns the number of times that the given name appears in the follows lists of all
      *  the users in this network. Note: A name can appear 0 or 1 times in each list. */
     private int followeeCount(String name) {
-        //// Replace the following statement with your code
-        return 0;
+        int count = 0;
+        for (int i = 0; i < userCount; i++) {
+            if (users[i].follows(name)) count++;
+        }
+        return count;
     }
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-       //// Replace the following statement with your code
-       return null;
+        String description = "";
+        for (int i = 0; i < userCount; i++) {
+            description += users[i] + "\n";
+        }
+        return description;
     }
 }
